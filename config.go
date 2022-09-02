23fea04bd9b6657f71bf4acf31bc61f7937fd33d
Kubacka Wojciech <wojciech.kubacka@grupawp.pl>
2022-09-02 10:13:42 +0200

    One small fix :)

--- config.go
+++ config.go
@@ -3,7 +3,7 @@ package rocheinteview
 type Config struct {
 	HTTPPort    int    `envconfig:"HTTP_PORT" default:"40000"`
 	GRPCPort    int    `envconfig:"GRPC_PORT" default:"30000"`
-	ServiceName string `envconfig:"SERVICE_NAME" default:"ROCHE"`
+	ServiceName string `envconfig:"SERVICE_NAME" default:"roche"`
 
 	Env string `envconfig:"env"`
 }
